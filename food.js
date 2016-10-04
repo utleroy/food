4728341061dbc664245c957be34b02eb23c514d7
utleroy <bryanhankins@me.com>
2016-10-03 22:32:33 -0500

    xhr displaying all products in DOM

--- food.js
+++ food.js
@@ -2,11 +2,32 @@ var dogFood = [];
 
 function loadFood () {
 	dogFood = JSON.parse(this.response).dog_brands;
-	var content = document.getElementById("output");
+	var contentElement = document.getElementById("output");
+	var displayDiv = "";
+	var types = [];
 	for (var i = 0; i < dogFood.length; i++) {
-		 var dogFoodLoop = dogFood[i];
+		var dogFoodLoop = dogFood[i];
+
+		displayDiv += 
+		`<div>
+		  <h1>${dogFoodLoop.name}</h1>
+		  <hr>
+		  <h2>${dogFoodLoop.types[0].type}</h2> 
+		  <h3>${dogFoodLoop.types[0].volumes[0].name}</h3> 
+		  <h3>$${dogFoodLoop.types[0].volumes[0].price}</h3>
+		  <h3>${dogFoodLoop.types[0].volumes[1].name}</h3> 
+		  <h3>$${dogFoodLoop.types[0].volumes[1].price}</h3>
+		  <h2>${dogFoodLoop.types[1].type}</h2>
+		  <h3>${dogFoodLoop.types[1].volumes[0].name}</h3> 
+		  <h3>$${dogFoodLoop.types[1].volumes[0].price}</h3>
+		  <h3>${dogFoodLoop.types[1].volumes[1].name}</h3> 
+		  <h3>$${dogFoodLoop.types[1].volumes[1].price}</h3> 
+		  <hr> 
+		 </div>`
+
+		contentElement.innerHTML = displayDiv;
 	}
-console.log("dogFoodLoop", dogFoodLoop);
+	console.log("dogFoodLoop", dogFoodLoop);
 }
 
 var dogBrandsReq = new XMLHttpRequest();
