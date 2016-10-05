b310c74bd3907296227f3f76743d9a108bf32006
utleroy <bryanhankins@me.com>
2016-10-04 21:18:40 -0500

    dog and cat food xhr's working

--- food.js
+++ food.js
@@ -4,26 +4,26 @@ function loadFood () {
 	dogFood = JSON.parse(this.response).dog_brands;
 	var contentElement = document.getElementById("output");
 	var displayDiv = "";
-	var types = [];
+	displayDiv += `<h1>DAWG STUFF</h1>`
 	for (var i = 0; i < dogFood.length; i++) {
 		var dogFoodLoop = dogFood[i];
 
 		displayDiv += 
 		`<div>
-		  <h1>${dogFoodLoop.name}</h1>
-		  <hr>
-		  <h2>${dogFoodLoop.types[0].type}</h2> 
-		  <h3>${dogFoodLoop.types[0].volumes[0].name}</h3> 
-		  <h3>$${dogFoodLoop.types[0].volumes[0].price}</h3>
-		  <h3>${dogFoodLoop.types[0].volumes[1].name}</h3> 
-		  <h3>$${dogFoodLoop.types[0].volumes[1].price}</h3>
-		  <h2>${dogFoodLoop.types[1].type}</h2>
-		  <h3>${dogFoodLoop.types[1].volumes[0].name}</h3> 
-		  <h3>$${dogFoodLoop.types[1].volumes[0].price}</h3>
-		  <h3>${dogFoodLoop.types[1].volumes[1].name}</h3> 
-		  <h3>$${dogFoodLoop.types[1].volumes[1].price}</h3> 
-		  <hr> 
-		 </div>`
+		<h2>${dogFoodLoop.name}</h2>
+		<hr>
+		<h2>${dogFoodLoop.types[0].type}</h2> 
+		<h3>${dogFoodLoop.types[0].volumes[0].name}</h3> 
+		<h3>$${dogFoodLoop.types[0].volumes[0].price}</h3>
+		<h3>${dogFoodLoop.types[0].volumes[1].name}</h3> 
+		<h3>$${dogFoodLoop.types[0].volumes[1].price}</h3>
+		<h2>${dogFoodLoop.types[1].type}</h2>
+		<h3>${dogFoodLoop.types[1].volumes[0].name}</h3> 
+		<h3>$${dogFoodLoop.types[1].volumes[0].price}</h3>
+		<h3>${dogFoodLoop.types[1].volumes[1].name}</h3> 
+		<h3>$${dogFoodLoop.types[1].volumes[1].price}</h3> 
+		<hr> 
+		</div>`
 
 		contentElement.innerHTML = displayDiv;
 	}
@@ -32,15 +32,30 @@ function loadFood () {
 
 function loadCatFood () {
 	catFood = JSON.parse(this.response).cat_brands;
-	var contentElement = document.getElementById("output");
-	var displayDiv = "";
-	var types = [];
+	var contentCatElement = document.getElementById("cat-output");
+	var catDisplayDiv = "";
+	catDisplayDiv += `<h1>CAT STUFF</h1>`
 	for (var i = 0; i < catFood.length; i++) {
 		var catFoodLoop = catFood[i];
+			
+		catDisplayDiv += 
+		`<div>
+		<h2>${catFoodLoop.name}</h2>
+		<hr>
+		<h2>${catFoodLoop.breeds[0]}</h2>
+		</div>`
 
-		displayDiv += 
+contentCatElement.innerHTML = catDisplayDiv;
+	}
+	console.log("catFoodLoop", catFoodLoop)
+}
 
 var catBrandsReq = new XMLHttpRequest();
 catBrandsReq.addEventListener("load", loadFood);
+catBrandsReq.open("GET", "dogfood.json");
+catBrandsReq.send();
+
+var catBrandsReq = new XMLHttpRequest();
+catBrandsReq.addEventListener("load", loadCatFood);
 catBrandsReq.open("GET", "catfood.json");
 catBrandsReq.send();
